5b2a6829f3814aaf91ff5f1bc3420d7f849b29aa
Sumanth <svaraganti@murraystate.edu>
2016-07-09 00:51:52 -0400

    maven

---
+++ src/main/java/Sample_Maven/Sample_Maven/App.java
@@ -0,0 +1,13 @@
+package Sample_Maven.Sample_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
